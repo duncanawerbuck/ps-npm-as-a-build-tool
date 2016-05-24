dee7885f8f569cfab09d7b9985d6f9ed0980c9b1
Awerbuck <dawerbuck@deloitte.ie>
2016-05-24 08:24:38 +0100

    Started TS compilation demo. Test fails cos we're not compiling ts yet.

---
+++ test/testingTypeScriptCode.js
@@ -0,0 +1,13 @@
+/* global describe it */
+
+var should = require('should'),
+    greeter = require('../src/typescript/tsCode.js');
+
+should.equal(1); //TODO: Got to be a better way to silence eslint whinging that should is never used.
+
+describe('TypeScript is compiled ok', function () {
+    it('when this passed', function () {
+        var g = new greeter();
+        g.greet('Yoda').should.equal('A type-scripting greeting to you, Yoda');
+    });
+});
